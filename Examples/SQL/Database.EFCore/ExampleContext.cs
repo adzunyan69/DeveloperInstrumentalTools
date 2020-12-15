f617856d61b0116343081d7a8d98e779a0994686
adzunyan69 <kruglakovartem@gmail.com>
2020-12-15 23:29:50 +0300

    Edit password for postgres

--- Examples/SQL/Database.EFCore/ExampleContext.cs
+++ Examples/SQL/Database.EFCore/ExampleContext.cs
@@ -23,7 +23,7 @@ namespace Database.EFCore
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;User ID=postgres;Password=root;");
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;User ID=postgres;Password=postgres;");
             }
         }
 
