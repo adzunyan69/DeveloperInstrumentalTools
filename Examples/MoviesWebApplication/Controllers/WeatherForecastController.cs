1a3cf6d2f73ec4eda3075cf937b19d2fa21ca154
adzunyan69 <kruglakovartem@gmail.com>
2020-12-22 23:16:07 +0300

    Задание 2

---
+++ Examples/MoviesWebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoviesWebApplication.Models;
+
+namespace MoviesWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IEnumerable<Movies> Get()
+        {
+            using (var _context  = new MoviesDBContext())
+            {
+                /*Movies movie = new Movies();
+                //movie.MoviesId = 1;
+                movie.Name = "First Title";
+                movie.Year = 2000;
+
+                _context.Movies.Add(movie);
+
+                _context.SaveChanges();*/
+                return _context.Movies.ToList();
+            }
+
+
+        }
+    }
+}
---
+++ Examples/MoviesWebApplication/Migrations/20201222201215_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace MoviesWebApplication.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MoviesId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Year = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MoviesId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MoviesId", "Name", "Year" },
+                values: new object[,]
+                {
+                    { 1, "First Title", 1990 },
+                    { 2, "Second Title", 2000 },
+                    { 3, "Third Title", 2020 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
